d441f6b07a20837b1cc47ebe19d04031d62139fe
alvinussutendy <alvinussutendy@gmail.com>
2018-11-03 23:16:03 +0700

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator{
+	
+	public Calculator(){
+		
+		
+	}
+	
+	public float multiply(float a, float b){
+		
+		
+	}
+	
+	public float division(float a, float b){
+		
+		
+	}
+}
