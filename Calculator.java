26e34eb11408841db06cce065ac5d65b5ec76a1b
alvinussutendy <alvinussutendy@gmail.com>
2018-11-08 15:00:34 +0700

    update calc

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,30 @@
 public class Calculator{
-	
+
 	public Calculator(){
-		
-		
+
+
 	}
-	
+
+	public float plus(float a , float b){
+
+
+	}
+
+	public float minus(float a , float b){
+
+
+	}
+
+
+
 	public float multiply(float a, float b){
-		
-		
+    return a*b;
+
 	}
-	
+
 	public float division(float a, float b){
-		
-		
+		return a/b;
+
+
 	}
 }
